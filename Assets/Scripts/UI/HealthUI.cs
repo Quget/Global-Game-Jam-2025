d2a7dba23fc656278c1186fc85dbb586c209453e
may-lisa <196335383+may-lisa@users.noreply.github.com>
2025-01-25 21:50:39 +0100

    UI/HUD integrated into gamedata

--- Assets/Scripts/UI/HealthUI.cs
+++ Assets/Scripts/UI/HealthUI.cs
@@ -5,50 +5,48 @@ using UnityEngine.UI;
 public class HealthUI : MonoBehaviour
 {
     public Image heart;
-    public int maxHealth = 3;
 
-    int currentHealth;
     GameObject[] hearts;
-
-    int Health
-    {
-        get => currentHealth;
-        set
-        {
-            currentHealth = Mathf.Clamp(value, 0, maxHealth);
-        }
-    }
 
     void Awake()
     {
         List<GameObject> hearts = new() { heart.gameObject };
 
-        for (int i = 0; i < maxHealth - 1; i++)
+        for (int i = 0; i < GameManager.Instance.gameData.Health - 1; i++)
             hearts.Add(Instantiate(heart.gameObject, transform));
 
         this.hearts = hearts.ToArray();
+    }
 
-        Health = maxHealth;
+    void Update()
+    {
+        if (hearts.Length > GameManager.Instance.gameData.Health)
+        {
+            RemoveHeart();
+        } else if (hearts.Length < GameManager.Instance.gameData.Health) {
+            AddHeart();
+        }
     }
 
     // Returns true if the health is zero
     public bool RemoveHeart()
     {
-        Health--;
+//        Health--;
         UpdateHearts();
 
-        return currentHealth <= 0;
+//        return currentHealth <= 0;
+        return GameManager.Instance.gameData.Health <= 0;
     }
 
     public void AddHeart()
     {
-        Health++;
+//        Health++;
         UpdateHearts();
     }
 
     void UpdateHearts()
     {
         for (int i = 0; i < hearts.Length; i++)
-            hearts[i].SetActive(Health > i);
+            hearts[i].SetActive(GameManager.Instance.gameData.Health > i);
     }
 }
