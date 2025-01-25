f29916245dd6bd67deb3463b1323c440147eaf67
may-lisa <196335383+may-lisa@users.noreply.github.com>
2025-01-25 20:06:16 +0100

    Item and power-up pick-up action

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -1,16 +1,38 @@
 using DubbelBubbel.Player;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Item : MonoBehaviour
 
 {
     private Player player;
+    private InputAction interactAction;
+    private float distanceToBeInRange = 2;
 
     void Awake()
     {
         player = FindFirstObjectByType<Player>();
         GameManager.Instance.gameData.AddItemToWorld(this);
+		interactAction = InputSystem.actions.FindAction("Interact");
+		interactAction.performed += InteractAction_performed;
     }
+
+	private void InteractAction_performed(InputAction.CallbackContext obj)
+	{
+        var distance = Vector3.Distance(player.transform.position, transform.position);
+		if (distance < distanceToBeInRange)
+		{
+            Debug.Log("Old Inventory: " + GameManager.Instance.gameData.Inventory.Count);
+            Debug.Log("Old Items in world: " + GameManager.Instance.gameData.ItemsInWorld.Count);
+            interactAction.performed -= InteractAction_performed;
+            GameManager.Instance.gameData.PickUpItem(this);
+            Destroy(this.gameObject);
+            Debug.Log("Item picked up");
+            Debug.Log("New Inventory: " + GameManager.Instance.gameData.Inventory.Count);
+            Debug.Log("New Items in world: " + GameManager.Instance.gameData.ItemsInWorld.Count);			
+		}
+	}
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -29,6 +51,7 @@ public class Item : MonoBehaviour
 		{
             Debug.Log("Old Inventory: " + GameManager.Instance.gameData.Inventory.Count);
             Debug.Log("Old Items in world: " + GameManager.Instance.gameData.ItemsInWorld.Count);
+            interactAction.performed -= InteractAction_performed;
             GameManager.Instance.gameData.PickUpItem(this);
             Destroy(this.gameObject);
             Debug.Log("New Inventory: " + GameManager.Instance.gameData.Inventory.Count);
