7b031c693291a6b72face5777cf7c8e07e6c10f0
may-lisa <196335383+may-lisa@users.noreply.github.com>
2025-01-25 10:43:08 +0100

    Update to gamedata, bubble and created item class

--- Assets/Scripts/GameData.cs
+++ Assets/Scripts/GameData.cs
@@ -1,33 +1,105 @@
+using System.Collections.Generic;
 using JetBrains.Annotations;
 using UnityEngine;
 
 public class GameData
 {
+    // maxBubbles: The max amount of bubbles that can be present in the game
+    private int maxBubbles = 3;
+    public int MaxBubbles
+    {
+        get {return maxBubbles; }
+        set {maxBubbles = value; }
+    }
+    // bubbleTimer: The life span of a bubble in seconds
+    private int bubbleTimer = 5;
+    public int BubbleTimer
+    {
+        get {return bubbleTimer; }
+        set {bubbleTimer = value; }
+    }
+
+    // bubbles: Array of the bubble objects currently in existence
+    private List<Bubble> bubbles;
+    public List<Bubble> Bubbles
+    {
+        get {return bubbles;}
+    }
+    public void SpawnBubble(Bubble bubble)
+    {
+        if (bubbles.Count < maxBubbles)
+        {
+            bubbles.Add(bubble);
+        }
+    }
+    // health: Starting health of the player
     private int health = 3;
     public int Health 
     {
         get {return health; }
+        set {health = value; }
     }
     public void GainHealth()
     {
+        Debug.Log("GainHealth - Old health: " + health);
         health += 1;
+        Debug.Log("GainHealth - New health: " + health);
     }
     public void LoseHealth()
     {
+        Debug.Log("LoseHealth - Old health: " + health);
         if (health != 0) {
             health -= 1;
+            Debug.Log("LoseHealth - New health: " + health);
         } else {
+            Debug.Log("Health can't go lower than 0. Game-over.");
             GameOver();
         }
     }
     public void GameOver() 
     {
-        // :) Oops we're dead
+        // :) Oj kurwa, we're dead
     }
-    private int maxBubbles = 3;
-    private int bubbleTimer = 5;
-    private bool powerUpActive = true;
-    public Bubble[] bubbles;
-    //public InventoryItem[] inventory;
 
+    /*
+    Power up system
+    0. Bubbles disabled
+    1. Character can shoot bubbles that can be jumped on
+    2. Bubbles affect enemies. Jumping on an enemy trapped in the bubble kills it.
+    3. Bubbles affect spikes. Player doesn't lose health when jumping on spikes covered by a bubble.
+    */
+    private int powerUp = 0;
+    public int PowerUp
+    {
+        get {return powerUp; }
+        set {powerUp = value; }
+    }
+
+    public void pickUpPowerUp()
+    {
+        powerUp += 1;
+    }
+
+    private List<Item> inventory;
+    public List<Item> Inventory{
+        get {return inventory; }
+        set {inventory = value; }
+    }
+
+    private List<Item> itemsInWorld;
+    public List<Item> ItemsInWorld {
+        get {return itemsInWorld; }
+        set {itemsInWorld = value; }
+    }
+
+    public void addItemToWorld()
+    {
+        itemsInWorld.Add(new Item());
+    }
+
+    public void pickUpItem(Item item)
+    {
+        inventory.Add(item);
+        itemsInWorld.Remove(item);
+    }
 }
