f82c8a97e541318301582393e9869026beef4a89
Ewoud <github@ewoudva.nderwerf.com>
2025-01-25 13:49:51 +0100

    updated scene and prefabs

--- Assets/Scripts/DownscaleCamera.cs
+++ Assets/Scripts/DownscaleCamera.cs
@@ -36,11 +36,20 @@ public class DownscaleCamera : MonoBehaviour
         canvas.renderMode = RenderMode.ScreenSpaceOverlay;
         canvas.vertexColorAlwaysGammaSpace = true;
 
+        Image backgroundImage = new GameObject("Background Bars").AddComponent<Image>();
+        backgroundImage.transform.SetParent(canvas.transform);
+        backgroundImage.transform.localPosition = Vector3.zero;
+        backgroundImage.rectTransform.sizeDelta = new Vector2(Camera.main.pixelWidth, Camera.main.pixelHeight);
+
+        backgroundImage.color = Color.black;
+
         RawImage renderImage = new GameObject("Pixel Result").AddComponent<RawImage>();
         renderImage.transform.SetParent(canvas.transform);
         renderImage.transform.localPosition = Vector3.zero;
 
         renderImage.rectTransform.sizeDelta = new Vector2(Camera.main.pixelWidth, Camera.main.pixelHeight);
+
+        renderImage.transform.localScale = new Vector3(((float)resolution.x / (float)resolution.y) / ((float)Camera.main.pixelWidth / (float)Camera.main.pixelHeight), 1f, 1f);
 
         renderImage.texture = pixelTexture;
     }
