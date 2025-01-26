6d10934712b9309b69d8a31c812343d0ed5ae067
Rose Quget <rose@quget.com>
2025-01-26 12:09:07 +0100

    Bug fix death

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using DubbelBubbel.Player;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,31 +10,35 @@ public class GameManager : MonoBehaviour
 
 	public GameData gameData;
 
-	private Player player;
-
 	private void Awake()
 	{
 		if(instance == null)
 		{
 			instance = this;
 			gameData = new GameData();
-			gameData.OnPlayerDeath += GameData_OnPlayerDeath;
-			player = Object.FindFirstObjectByType<Player>();
 			DontDestroyOnLoad(gameObject);
 		}
 	}
-
-	private void OnDestroy()
+	// called second
+	void OnEnable()
 	{
-		if(instance == this)
-		{
-			gameData.OnPlayerDeath -= GameData_OnPlayerDeath;
-		}
+		gameData.OnPlayerDeath += GameData_OnPlayerDeath;
+	}
+
+	void OnDisable()
+	{
+		gameData.OnPlayerDeath -= GameData_OnPlayerDeath;
+	}
+
+	public void EndGame()
+	{
+		Debug.Log("End Game");
 	}
 
 	private void GameData_OnPlayerDeath(object sender, System.EventArgs e)
 	{
-		if (!player.IsDestroyed())
+		var player = Object.FindFirstObjectByType<Player>();
+		if (player != null && !player.IsDestroyed())
 		{
 			Destroy(player.gameObject);
 		}
